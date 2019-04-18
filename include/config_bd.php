4c20318e1f3724e7f5f24ee511e367c81f0915d7
dark452 <dark452@users.noreply.github.com>
2019-04-17 22:47:39 -0400

    Update config_bd.php

--- include/config_bd.php
+++ include/config_bd.php
@@ -6,8 +6,8 @@
  */
 
 $usuario    = "BD_VTAFAC";
-$contrasena = "factur11";
-$urlDatos   = "89.168.89.105:1521/prod11"; //Polapy
+$contrasena = "XXXXXX";
+$urlDatos   = "XX.XXX.XX.XXX:1521/prod11"; //Polapy
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 ?>
